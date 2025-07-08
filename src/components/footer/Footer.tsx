85014d0f6e381a8eb81e3667974528eec6b975f3
Natan Macedo <natan.mac.n@gmail.com>
2025-07-08 19:53:08 -0300

    :sparkles: implementado login + visualização de Categorias

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,41 @@
+
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { useContext, type ReactNode } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    //Isso serve para mostrar um componente de maneira condicional
+    //Na tela de login, navbar e footer n aparece, e quando faz o login aparece
+    if(usuario.token !== ""){
+        component = (
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Delivery Valor | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <LinkedinLogo size={48} weight='bold' />
+                        <InstagramLogo size={48} weight='bold' />
+                        <FacebookLogo size={48} weight='bold' />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            { component }
+        </>
+    )
+}
+
+export default Footer
