db927e17ce32d604e7d7c5dcc429ea03847cc87b
luizsantos7 <lhsantos098@gmail.com>
2025-07-11 13:40:56 -0300

    deploy vercel

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -6,8 +6,8 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    //baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'https://delivery-hzm2.onrender.com',
+    baseURL: import.meta.env.VITE_API_URL,
+    //baseURL: 'https://delivery-hzm2.onrender.com',
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
